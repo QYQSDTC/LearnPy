fb472d77157ea228b53d1acf3b9a99b684fdc623
Dr.QYQ <qyqstc@gmail.com>
2017-07-14 00:08:30 +0800

    New repository

---
+++ LearnPy/ex.py
@@ -0,0 +1,63 @@
+## exercise 1
+print("Hello QYQ")
+print("Hello QYQ again!")
+print('ahhhhhh')
+print("It is 'fun'")
+print('see the different "point"')
+print("I am genius Dr.QYQ")
+## exercise 2
+print("I will now count my chickens:")
+print("Hens:",25+40/5)
+print("Rosters:",10%4)
+print("Now I will count eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2)
+print( "is it true that 3 + 2 < 5 - 7")
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2", 3 + 2)
+print("What is 5 - 7",5 - 7)
+print("That is why it is false")
+## exercise 7
+a = "yes"
+print("Am I so genius?{}".format('yes'))
+print(f"Am I so genius again?{a}")
+print("Who I am?",end=' ')
+print("You are Dr.QYQ")
+## exercise 10
+print("I am 6'10\" tall")
+print("  I \n have \n so \n many \n words \n to \n say")
+print("""
+you
+  can
+    say
+      like
+        what
+     you like !
+     """)
+print('''
+you
+  can
+    say
+      like
+        what
+     you like !
+     ''')
+## exercise 11
+print("How old are you?",end=' ')
+age=input()
+print("What's your name?",end=' ')
+name=input()
+print("How much do you weight?",end=' ')
+weight=input()
+print(f"So you are {name}, you are {age} old and {weight} heavy")
+## exercise 13
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+script=input()
+print("Your first virable is:", first)
+first=input()
+print("Your second virable is:", second)
+second=input()
+print("Your third virable is:", third)
+third=input()
---
+++ LearnPy/ex13.py
@@ -0,0 +1,14 @@
+from sys import argv
+script, user_name = argv
+prompt = '>'
+print(f"Hi {user_name}, I am the {script} script")
+print("I'd like to ask you some questions")
+print(f"Do you like me? {user_name}")
+likes = input(prompt)
+print(f"Where do you live? {user_name}")
+lives = input(prompt)
+print("What computer do you have?")
+computer = input(prompt)
+print(f'''
+    Allright, so you said \"{likes}\" liking me. You live in {lives}). Not sure where that is. You have a {computer} computer. Nice.
+    ''')
