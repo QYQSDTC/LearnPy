50f64aea9ff8557db9252b645cbfdc59daf9f4d4
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2020-10-13 19:47:57 +0800

    creat Path.py to learn how to set path

--- Py excercise/Path.py
+++ Py excercise/Path.py
@@ -3,9 +3,12 @@ Author: Yiqian Qian
 Description: Lear how does path work.
 Date: 2020-10-13 19:24:47
 LastEditors: Yiqian Qian
-LastEditTime: 2020-10-13 19:28:21
+LastEditTime: 2020-10-13 19:46:53
 FilePath: /undefined/Users/qyq/Development/LearnPy/Py excercise/Path.py
 '''
 from os import path
-filepath = path.join(path.dirname(__file__),"resources")
-print(filepath)
+filepath1 = path.dirname(__file__)
+print(f"dirname is:{filepath1}")
+
+filepath2 = path.realpath(__file__)
+print("real path is:{}".format(filepath2))
